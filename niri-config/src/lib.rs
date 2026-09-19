9179f96101ea29ffaac01d4434afb9c4a61672c3
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2026-09-19 11:15:22 +0800

    force rendering options

--- niri-config/src/lib.rs
+++ niri-config/src/lib.rs
@@ -1958,6 +1958,7 @@ mod tests {
                         ),
                     ),
                     tiled_state: None,
+                    force_render: None,
                     background_effect: BackgroundEffectRule {
                         xray: None,
                         blur: None,
